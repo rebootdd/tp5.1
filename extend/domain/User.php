606af57759d83d741739d1a652b47a9d112539bf
rebootdd <1574821559@qq.com>
2018-10-29 15:55:30 +0800

    extend/下文件被忽略了，由于.gitignore文件中的配置了*(忽略全部文件)

---
+++ extend/domain/User.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reboot
+ * Date: 2018/10/29 0029
+ * Time: 14:23
+ */
+
+namespace domain;
+
+
+class User
+{
+    /**
+     * Notes: 获取用户信息
+     * User: reboot
+     * Date: 2018/10/29 0029
+     * Time: 14:23
+     */
+    public static function getUserInfo() {
+        return '自定义的扩展类';
+    }
+}
